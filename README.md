8a40091e3402e6f2f1da2b539700c965283814a4
root <root@ip-172-31-86-158.ec2.internal>
2022-06-21 15:25:09 +0000

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# north-star
+# North-star
+
+## Pre-requisites
+## Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+## Install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+## Install AWS cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+## Configure AWS cli
+aws configure set aws_access_key_id $access_key
+aws configure set aws_secret_access_key $secret_key
+
+## Install Terraform
+https://learn.hashicorp.com/tutorials/terraform/install-cli
+
+
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+##Variables
+namespace="ns"
+
+export HELM_NAMESPACE=$namespace
+
+##Add helm repo
+helm repo add jenkins https://charts.jenkins.io
+helm repo add sonarqube https://SonarSource.github.io/helm-chart-sonarqube
+helm repo add sonatype https://sonatype.github.io/helm3-charts/
+
+helm repo update
+
+##Install Jenkins
+helm upgrade --install jenkins --namespace $namespace -f helm/jenkins/values.yaml jenkins/jenkins --create-namespace --wait
+
+##Install Sonarqube
+helm upgrade --install sonarqube --namespace $namespace -f helm/sonarqube/values.yaml sonarqube/sonarqube --create-namespace --wait
+
+##Install Nexus-iq-server
+helm upgrade --install nexus-iq --namespace $namespace -f helm/nexus-iq/values.yaml sonatype/nexus-iq-server [ --version v90.0.0 ] --create-namespace --wait
+
+
