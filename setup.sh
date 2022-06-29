c41fcc0f848aa14b140ef58bf63bc40d6d3a0fc1
root <root@ip-172-31-36-233.ap-south-1.compute.internal>
2022-06-29 05:58:40 +0000

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,50 @@
-#!/bin/sh
+#!/bin/bash
+echo -e "Initiating....."
+sleep 3
+
+echo -e "\e[1;32m"
+echo -e "*****************************************************************************"
+echo -e "#  _   _                                       __                           #  "
+echo -e "# | \ | |                   _     _          / __)    _                     # "
+echo -e "# |  \| |   ___    _  _   _| |_  | |_       ( (_    _| |_    ___      _  _  # "
+echo -e "# |     |  / _ \  | |/ / |_   _| | '_ \      \__ \ |_   _|  / _ \    | |/ / #   "
+echo -e "# | |\  | | (_) | | |_/    | |_  | | | |      __) )  | |_  | (_) '\  | |_/  #   "
+echo -e "# |_| \_|  \___/  |_|      |___| |_| |_|     (__ /   |___|  \___/\_\ |_|    #   "
+echo -e "#                                                                           # "
+echo -e "*****************************************************************************"
+echo -e "\e[0m"
+
+sleep 3
+
 ##Variables
 namespace="devops"
 
 export HELM_NAMESPACE=$namespace
+
+##Stage 1 - Kubernetes cluster provisioning using terraform
+echo -e "\e[1;31m"
+echo -e "Stage 1"
+sleep 1
+echo -e "Kubernetes cluster provisioning using terraform"
+echo -e "\e[0m"
+sleep 1
+
+echo -e "Skipping for now............."
+
+#cd terraform
+#terraform init
+#terraform plan
+#terraform validate
+#terraform apply -auto-approve
+#cd ..
+
+##Stage 2 - Jenkins provisioning using Helm
+echo -e "\e[1;31m"
+echo -e "Stage 2"
+sleep 1
+echo -e "Jenkins provisioning using helm"
+echo -e "\e[0m"
+sleep 1
 
 ##Add helm repo
 helm repo add jenkins https://charts.jenkins.io
